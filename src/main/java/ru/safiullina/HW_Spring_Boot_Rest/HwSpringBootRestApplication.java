2c44c228ae0401e7310f3821393faf7d55421ef6
NataliaSafiullina <nfsafiullina@gmail.com>
2025-01-15 20:35:41 +0700

    Initial commit

---
+++ src/main/java/ru/safiullina/HW_Spring_Boot_Rest/HwSpringBootRestApplication.java
@@ -0,0 +1,13 @@
+package ru.safiullina.HW_Spring_Boot_Rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSpringBootRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwSpringBootRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/safiullina/HW_Spring_Boot_Rest/HwSpringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.safiullina.HW_Spring_Boot_Rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSpringBootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
